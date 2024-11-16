a15e66accfe3fc4e815ebf7bed5edcf86fe18ae1
juanjolr7 <juanjolr6@gmail.com>
2024-11-15 18:51:39 -0600

    ajustes

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+
+  private apiUrl = 'https://api.escuelajs.co/api/v1/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+  // loadCompleted() {
+  //   return this.httpclient.get(`${this.url}loadCompleted.php`);
+  // }
+}
